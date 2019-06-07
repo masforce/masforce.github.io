80b11ba34eee78a5778baa106b01f0f3c66ab8c6
Mason Force <43055846+masforce@users.noreply.github.com>
2019-06-07 13:12:36 -0700

    Add files via upload

--- siteScript.js
+++ siteScript.js
@@ -1,3 +1,55 @@
+$( document ).ready(function() {
+    
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        loadCondensed();
+    }
+    else {
+        
+        var state;
+        if ($(window).width() <= 1000) {
+            loadCondensed();
+            state = "condensed"
+        }
+        else {
+            state = "wide"
+        }
+        console.log("Page loaded in " + state + " format");
+        
+        $( window ).resize(function() {
+            if ($(window).width() <= 1000 && state == "wide") {
+                loadCondensed();
+                state = "condensed"
+            }
+            else if ($(window).width() >= 1000 && state == "condensed"){
+                loadWide();
+                state = "wide"
+            }
+        });
+    }
+    
+    function loadCondensed() {
+        console.log("switch to mobile layout!");
+        document.getElementById('wrapper').id = "wrapperCondensed"
+        document.getElementById('wrapper3').id = "wrapper3Condensed"
+        document.getElementById('subWrapper1').id = "subWrapper1Condensed"
+        document.getElementById('subWrapper2').id = "subWrapper2Condensed"
+        document.getElementById('socialMedia').id = "socialMediaCondensed"
+    }
+    
+    function loadWide() {
+        console.log("switch to wide layout!")
+        document.getElementById('wrapperCondensed').id = "wrapper"
+        document.getElementById('wrapper3Condensed').id = "wrapper3"
+        document.getElementById('subWrapper1Condensed').id = "subWrapper1"
+        document.getElementById('subWrapper2Condensed').id = "subWrapper2"
+        document.getElementById('socialMediaCondensed').id = "socialMedia"
+    }
+  
+    
+});
+
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
@@ -23,3 +75,68 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+
+$( document ).ready(function() {
+        
+        $('#sponsorCarousel').carousel({interval: 3900});
+
+        $('.carousel .item').each(function(){
+            
+          var next = $(this).next();
+            
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+            
+          next.children(':first-child').clone().appendTo($(this));
+            
+          if (next.next().length>0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+          }
+            
+          else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+          }
+            
+        });
+
+        $(window).scroll(function() {
+            
+           var tophT = $('#top').offset().top,
+               tophH = $('#top').outerHeight(),
+               
+               activitieshT = $('#projects').offset().top,
+               activitieshH = $('#projects').outerHeight(),
+               
+               experiencehT = $('#experience').offset().top,
+               experiencehH = $('#experience').outerHeight(),
+               
+               wH = $(window).height(),
+               wS = $(this).scrollTop();
+            
+            
+           if (wS == 0){
+               
+            $('a[href="#experience"]').css("background-color", "#00ADB5");
+            $('a[href="#top"]').css("background-color", "grey");
+            $('a[href="#projects"]').css("background-color", "#00ADB5");
+           }
+            
+           else if (wS > (activitieshT+activitieshH-(wH/2))){
+               
+            $('a[href="#experience"]').css("background-color", "#00ADB5");
+            $('a[href="#top"]').css("background-color", "#00ADB5");
+            $('a[href="#projects"]').css("background-color", "grey");
+           }
+            
+           else if (wS > (experiencehT+experiencehH-(wH/2))){
+               
+            $('a[href="#experience"]').css("background-color", "grey");
+            $('a[href="#top"]').css("background-color", "#00ADB5");
+            $('a[href="#projects"]').css("background-color", "#00ADB5");
+           }
+
+        });    
+
+    });
